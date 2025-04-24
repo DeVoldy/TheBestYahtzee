1868e60a9a2c38ce9c6ff406441cea3173fdc014
Davi Ferreira <ferreira.daviff@gmail.com>
2025-04-24 14:26:05 -0300

    função guardar_dado

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,10 @@ def rolar_dados(n):
     for i in range(n):
         lista_dados.append(random.randint(1,6))
     return lista_dados
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dados_rolados_atualizado = dados_rolados
+    dados_no_estoque_atualizado = dados_no_estoque
+    dados_no_estoque_atualizado.append(dados_rolados[dado_para_guardar])
+    del dados_rolados_atualizado[dado_para_guardar]
+    return [dados_rolados_atualizado,dados_no_estoque_atualizado]
