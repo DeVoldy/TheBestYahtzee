2fb58771a092f68fcd1cdae018c2def165c72669
Davi Ferreira <ferreira.daviff@gmail.com>
2025-04-24 14:32:03 -0300

    função remover_dado

--- funcoes.py
+++ funcoes.py
@@ -12,3 +12,10 @@ def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     dados_no_estoque_atualizado.append(dados_rolados[dado_para_guardar])
     del dados_rolados_atualizado[dado_para_guardar]
     return [dados_rolados_atualizado,dados_no_estoque_atualizado]
+
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
+    dados_rolados_atualizado = dados_rolados
+    dados_no_estoque_atualizado = dados_no_estoque
+    dados_rolados.append(dados_no_estoque[dado_para_remover])
+    del dados_no_estoque_atualizado[dado_para_remover]
+    return [dados_rolados_atualizado,dados_no_estoque_atualizado]
