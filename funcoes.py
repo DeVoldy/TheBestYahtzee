daef3b45f18977fbbecbdfcadc902e4d8eab5769
Davi Ferreira <ferreira.daviff@gmail.com>
2025-04-25 23:29:10 -0300

    iteração rolar_dados

--- funcoes.py
+++ funcoes.py
@@ -1,10 +1,7 @@
 import random
 
 def rolar_dados(n):
-    lista_dados = []
-    for i in range(n):
-        lista_dados.append(random.randint(1,6))
-    return lista_dados
+    return [random.randint(1,6) for i in range(n)]
 
 def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     dados_rolados_atualizado = dados_rolados
