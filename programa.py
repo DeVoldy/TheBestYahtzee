b879b36c860f9a22bff83c60475e384220dfcad2
CarlosNeto242 <carloslmn@al.insper.edu.br>
2025-04-30 17:15:45 -0300

    Programa

--- programa.py
+++ programa.py
@@ -1,4 +1,5 @@
 import funcoes as fun
+import random
 
 # Cartela inicial vazia
 cartela_de_pontos = {
@@ -21,7 +22,6 @@ cartela_de_pontos = {
 }
 
 fun.imprime_cartela(cartela_de_pontos)
-
 rolagem = fun.rolar_dados(5)
 print(f'Dados rolados: {rolagem}')
 guardados = []
@@ -29,42 +29,80 @@ print(f'Dados guardados: {guardados}')
 
 print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
 ordem = input('>')
-
-if ordem == '4':
-    fun.imprime_cartela(cartela_de_pontos)
-elif ordem == '1':
-    print('Digite o índice do dado a ser guardado (0 a 4):')
-    guardar = int(input('>'))
-    lista_provisoria = fun.guardar_dado(rolagem, guardados, guardar)
-    rolagem = lista_provisoria[0]
-    guardados = lista_provisoria[1]
-    print(f'Dados rolados: {rolagem}')
-    print(f'Dados guardados: {guardados}')
-elif ordem == '2':
-    print("Digite o índice do dado a ser removido (0 a 4):")
-    remover = int(input(">"))
-    lista_provisoria = fun.remover_dado(rolagem,guardados,remover)
-    rolagem = lista_provisoria[0]
-    guardados = lista_provisoria[1]
-    print(f'Dados rolados: {rolagem}')
-    print(f'Dados guardados: {guardados}')
-elif ordem == '3':
-    n = len(rolagem)
-    rolagem = fun.rolar_dados(n)
-    print(f'Dados rolados: {rolagem}')
-    print(f"Dados guardados: {guardados}")
-elif ordem =="0":
-    print("Digite a combinação desejada:")
-    combinacao = input(">")
-    if combinacao not in cartela_de_pontos['regra_simples'] and combinacao not in cartela_de_pontos['regra_avancada']:
-        print("Combinação inválida. Tente novamente.")
-    if combinacao in cartela_de_pontos['regra_simples'] and cartela_de_pontos['regra_simples'][combinacao] != -1:
-        print('Essa combinação já foi utilizada.')
-    if combinacao in cartela_de_pontos['regra_avancada'] and cartela_de_pontos['regra_avancada'][combinacao] != -1:
-        print('Essa combinação já foi utilizada.')
-    atualizado = fun.faz_jogada(rolagem+guardados,combinacao,cartela_de_pontos)
-    cartela_de_pontos = atualizado
-    fun.imprime_cartela(cartela_de_pontos)
-else:
-    print('Opção inválida. Tente novamente.')
-    ordem = int(input('>'))
+i = 0
+rerrolar = 0
+while i < 12:
+    if str(ordem) == '4':
+        fun.imprime_cartela(cartela_de_pontos)
+        print(f'Dados rolados: {rolagem}')
+        print(f'Dados guardados: {guardados}')
+        print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
+        ordem = input('>')
+    elif str(ordem) == '1':
+        print('Digite o índice do dado a ser guardado (0 a 4):')
+        guardar = int(input('>'))
+        lista_provisoria = fun.guardar_dado(rolagem, guardados, guardar)
+        rolagem = lista_provisoria[0]
+        guardados = lista_provisoria[1]
+        print(f'Dados rolados: {rolagem}')
+        print(f'Dados guardados: {guardados}')
+        print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
+        ordem = input('>')
+    elif str(ordem) == '2':
+        print("Digite o índice do dado a ser removido (0 a 4):")
+        remover = int(input(">"))
+        lista_provisoria = fun.remover_dado(rolagem,guardados,remover)
+        rolagem = lista_provisoria[0]
+        guardados = lista_provisoria[1]
+        print(f'Dados rolados: {rolagem}')
+        print(f'Dados guardados: {guardados}')
+        print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
+        ordem = input('>')
+    elif str(ordem) == '3':
+        if rerrolar < 2:
+            n = len(rolagem)
+            rolagem = fun.rolar_dados(n)
+            print(f'Dados rolados: {rolagem}')
+            print(f"Dados guardados: {guardados}")
+            print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
+            ordem = input('>')
+            rerrolar += 1
+        else:
+            print("Você já usou todas as rerrolagens.")
+            print(f'Dados rolados: {rolagem}')
+            print(f"Dados guardados: {guardados}")
+            print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
+            ordem = input(">")
+    elif str(ordem) == '0':
+        print("Digite a combinação desejada:")
+        combinacao = input(">")
+        while combinacao not in str(cartela_de_pontos['regra_simples']) and combinacao not in cartela_de_pontos['regra_avancada']:
+            print("Combinação inválida. Tente novamente.")
+            combinacao = input(">")
+        while combinacao in str(cartela_de_pontos['regra_simples']) and cartela_de_pontos['regra_simples'][int(combinacao)] != -1:
+            print('Essa combinação já foi utilizada.')
+            combinacao = input(">")
+        while combinacao in cartela_de_pontos['regra_avancada'] and cartela_de_pontos['regra_avancada'][combinacao] != -1:
+            print('Essa combinação já foi utilizada.')
+            combinacao = input(">")
+        atualizado = fun.faz_jogada(rolagem+guardados,combinacao,cartela_de_pontos)
+        cartela_de_pontos = atualizado
+        i += 1
+        if i != 12:
+            rerrolar = 0
+            rolagem = fun.rolar_dados(5)
+            print(f'Dados rolados: {rolagem}')
+            guardados = []
+            print(f'Dados guardados: {guardados}')
+            print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
+            ordem = input('>')
+    else:
+        print('Opção inválida. Tente novamente.')
+        ordem = int(input('>'))
+soma = 0
+for pontos in cartela_de_pontos["regra_simples"].values(): 
+    soma += pontos
+for pontos in cartela_de_pontos["regra_avancada"].values():
+    soma += pontos
+fun.imprime_cartela(cartela_de_pontos)
+print(f"Pontuação total: {soma}")
